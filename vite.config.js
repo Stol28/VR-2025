de7007e0e81f30bf96f6d84f5fb642b7e6828f1d
Steve Pasche <steve.pasche@heig-vd.com>
2025-03-04 15:20:46 +0100

    deploy action

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ const fullReloadPlugin = {
 
 export default defineConfig(({ command, mode }) => {
   const config = {
-    base: "/aframe-vue-boilerplate/",
+    base: "/VR-2025/",
     plugins: [
       basicSsl(),
       vue({
